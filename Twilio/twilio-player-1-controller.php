4d5fe083efe287600d06f86c4b94ecc650fc4256
Travis Chen <travis@scopely.com>
2012-09-05 22:13:09 -0700

    Update

--- Twilio/twilio-player-1-controller.php
+++ Twilio/twilio-player-1-controller.php
@@ -1,7 +1,35 @@
 <?php
+
+	$control = "None";
+	$phone_number = $_REQUEST['From'];
+	$body = $_REQUEST['Body'];
+	$body = strtolower($body);
+	$message = "Player 1: Control not understood. Try (U)p, (D)own, (L)eft or (R)ight.";
+	
+	if (strpos($body,'l') !== false) {
+	    $control = "Left";
+	}
+	
+	if (strpos($body,'r') !== false) {
+	    $control = "Right";
+	}
+	
+	if (strpos($body,'u') !== false) {
+	    $control = "Up";
+	}
+
+	if (strpos($body,'d') !== false) {
+	    $control = "Down";
+	}
+	
+	if( $control != "None" )
+	{
+		$message = "Player 1: Moving $control";
+	}
+
 	header("content-type: text/xml");
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-	<Sms>Ready Player 1</Sms>
+	<Sms><?php echo $message ?></Sms>
 </Response>
