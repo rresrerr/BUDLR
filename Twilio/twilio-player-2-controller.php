5190373a8f2c59932b39481ab0e78f208f0f1c9e
Travis Chen <travis@scopely.com>
2012-09-05 23:27:30 -0700

    First mysql update

--- Twilio/twilio-player-2-controller.php
+++ Twilio/twilio-player-2-controller.php
@@ -1,34 +1,6 @@
 <?php
-
-	$control = "None";
-	$phone_number = $_REQUEST['From'];
-	$body = $_REQUEST['Body'];
-	$body = strtolower($body);
-	$message = "Player 2: Control not understood. Try (U)p, (D)own, (L)eft or (R)ight.";
-	
-	if (strpos($body,'l') !== false) {
-	    $control = "Left";
-	}
-	
-	if (strpos($body,'r') !== false) {
-	    $control = "Right";
-	}
-	
-	if (strpos($body,'u') !== false) {
-	    $control = "Up";
-	}
-
-	if (strpos($body,'d') !== false) {
-	    $control = "Down";
-	}
-	
-	if( $control != "None" )
-	{
-		$message = "Player 2: Moving $control";
-	}
-
-	header("content-type: text/xml");
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	$player = 2;
+	include 'twilio-player-shared-controller.php';
 ?>
 <Response>
 	<Sms><?php echo $message ?></Sms>
---
+++ Twilio/twilio-player-shared-controller.php
@@ -0,0 +1,53 @@
+<?php
+	$control = "None";
+	$phone_number = $_REQUEST['From'];
+	$body = $_REQUEST['Body'];
+	$body = strtolower($body);
+	$id = $_GET['id'];
+	$message = "Player $player: Control not understood. Try (U)p, (D)own, (L)eft or (R)ight.";
+	
+	if (strpos($body,'l') !== false) {
+	    $control = "Left";
+	}
+	
+	if (strpos($body,'r') !== false) {
+	    $control = "Right";
+	}
+	
+	if (strpos($body,'u') !== false) {
+	    $control = "Up";
+	}
+
+	if (strpos($body,'d') !== false) {
+	    $control = "Down";
+	}
+	
+	if( $control != "None" )
+	{
+		$message = "Player $player: Moving $control";
+		
+		// Send control to server
+		if ( !mysql_connect("mysql.travis.aristomatic.com","travischen","ghandi") )
+		{
+			$message = "MYSQL ERROR: could not connect to mysel server.";
+		}
+		else
+		{
+			if( !mysql_select_db("disrupt_controls") )
+			{
+				$message = "MYSQL ERROR: can't select DB.";	
+			}
+			else
+			{
+				$query=" INSERT INTO controls (id, player, control ) VALUES ('$id', '$player', '$control')";
+				if ( !mysql_query($query) )
+				{
+					$message = "MYSQL ERROR: query failed.";
+				}
+			}
+		}
+	}
+
+	header("content-type: text/xml");
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
